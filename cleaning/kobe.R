dafbc1ef29c9ae3880552935233844ad5bd9eaea
AkiShiroshita <akihirokun8@gmail.com>
2022-01-24 09:01:00 +0000

    kobe lab and culture

--- cleaning/kobe.R
+++ cleaning/kobe.R
@@ -11,3 +11,32 @@ dpcdwh %>% colnames()
 
 dpc <- dpcdwh %>% 
   select()
+
+
+# Lab ---------------------------------------------------------------------
+
+lab <- read_excel("data/kobe/uncleaned/lab.xlsx")
+lab %>% glimpse()
+lab %>% colnames()
+
+lab <- lab %>% 
+  rename(id = "研究用ID",
+         adm_date = "入院日",
+         date = "検査日",
+         name = "検査項目名称",
+         value = "結果値") %>% 
+  select(id, adm_date, date, name, value)
+
+# Culture -----------------------------------------------------------------
+
+culture <- read_excel("data/kobe/uncleaned/culture.xlsx")
+culture %>% glimpse()
+culture %>% colnames()
+
+culture <- culture %>% 
+  rename(id = "研究用ID",
+         adm_date = "入院日",
+         date = "検体採取日時",
+         name = "検体名",
+         value = "菌名称") %>% 
+  select(id, adm_date, date, name, value)
