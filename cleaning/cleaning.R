b3abb6b1a0b30373a43dff965b3f114e1db890ea
AkiShiroshita <55659285+AkiShiroshita@users.noreply.github.com>
2022-03-03 22:57:29 +0900

    Update cleaning.R

--- cleaning/cleaning.R
+++ cleaning/cleaning.R
@@ -33,6 +33,15 @@ usethis::git_sitrep()
 usethis::create_github_token()
 gitcreds::gitcreds_set()
 
+source(here("cleaning","chart_clean.R"),
+       encoding="UTF-8")
+source(here("cleaning","dpc_rename.R"),
+       encoding="UTF-8")
+source(here("cleaning","dpc_select.R"),
+       encoding="UTF-8")
+source(here("cleaning","uniter.R"),
+       encoding="UTF-8")
+
 # Ichinomiya --------------------------------------------------------------
 
 file.edit("cleaning/ichinomiya.R")
